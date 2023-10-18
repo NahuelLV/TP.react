e053f4dbf068928f25a6208aa52aa2c058f66e15
NahuelLV <130933110+NahuelLV@users.noreply.github.com>
2023-10-18 17:59:56 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,14 @@
-import React from 'react'
+import {React, UseState, UseEfect} from 'react'
 import ReactDOM from 'react-dom/client'
 import { Link, Routes, Route, BrowserRouter } from 'react-router-dom';
-import App from './HOME.jsx'
-import Comentario from './comentarios'
-import Publica from './publicar'
-import Nav from './navegar'
-import Admin from './Admin.jsx';
-import './HOME.css'
+import App from './App.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div className='main'>
   <React.StrictMode>
     <BrowserRouter>  
-    <Nav />
-    <Routes>
-    <Route  exact path='/' Component={App} /> 
-        <Route exact path='/publicar' Component={Publica} />
-        <Route exact path='/comentarios' Component={Comentario} />
-        <Route exact path='/Admin' Component={Admin} />
-        <Route exact path='/comentarios/:id' element={<Comentario/>} />
-      </Routes>
+    <App />
     </BrowserRouter>
   </React.StrictMode>,
   </div>
